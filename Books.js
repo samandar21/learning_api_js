5ac89200c73ad717b3e89fef90e27d40ca08c17f
XolmominovSamandar <135332607+samandar21@users.noreply.github.com>
2024-03-14 14:00:10 +0500

    Create Books.js

---
+++ Books.js
@@ -0,0 +1,22 @@
+const books=[
+    {
+        id:1,
+        name:"Alximik",
+        author:"Paolo Koelyo",
+        pages:150
+    },
+    {
+        id:2,
+        name:"Shaytanat",
+        author:"Tohir Malik",
+        pages:365
+    },
+    {
+        id:3,
+        name:"Sariq devni minib",
+        author:"Xudoyberdi To'xtaboyev",
+        pages:290
+    }
+]
+
+module.exports=books
